792261227ef7a966ce8a708dd03fddd0487e2af2
Abdullah-1922 <kafikafi1922@gmail.com>
2025-01-31 15:11:33 +0600

    and, or ,filtering in prisma

---
+++ src/filtering.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const filtering = async () => {
+  const andFiltering = await prisma.post.findMany({
+    where: {
+      AND: [
+        {
+          title: {
+            contains: "title",
+          },
+        },
+        {
+          published: true,
+        },
+      ],
+    },
+  });
+//   console.log("And filtering", andFiltering);
+  const orFiltering = await prisma.post.findMany({
+    where: {
+      OR: [
+        {
+          title: {
+            contains: "title",
+          },
+        },
+        {
+          published: true,
+        },
+      ],
+    },
+  });
+  console.log("Or filtering", orFiltering);
+};
+filtering();
